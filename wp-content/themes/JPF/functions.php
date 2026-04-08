234b2941ca5426dfec8f29527d2ce76802120706
田中丈治 <jpf-30568@leto.eonet.ne.jp>
2026-04-08 18:41:33 +0900

    Fix English HOME menu link too

--- wp-content/themes/JPF/functions.php
+++ wp-content/themes/JPF/functions.php
@@ -3,15 +3,15 @@
 add_filter( 'wp_nav_menu_objects', 'jpf_fix_home_menu_links', 10, 2 );
 
 function jpf_fix_home_menu_links( $items, $args ) {
-    $home_url = home_url( '/' );
-    $legacy_paths = array(
-        home_url( '/top/' ),
-        home_url( '/?page_id=8' ),
+    $legacy_url_map = array(
+        home_url( '/top/' )     => home_url( '/' ),
+        home_url( '/?page_id=8' ) => home_url( '/' ),
+        home_url( '/en/top-2/' ) => home_url( '/en/' ),
     );
 
     foreach ( $items as $item ) {
-        if ( in_array( $item->url, $legacy_paths, true ) ) {
-            $item->url = $home_url;
+        if ( isset( $legacy_url_map[ $item->url ] ) ) {
+            $item->url = $legacy_url_map[ $item->url ];
         }
     }
 
@@ -28,8 +28,13 @@ function jpf_redirect_legacy_top_page() {
     $request_uri = isset( $_SERVER['REQUEST_URI'] ) ? wp_unslash( $_SERVER['REQUEST_URI'] ) : '';
     $page_id = isset( $_GET['page_id'] ) ? sanitize_text_field( wp_unslash( $_GET['page_id'] ) ) : '';
 
-    if ( '/top/' === untrailingslashit( $request_uri ) . '/' || '8' === $page_id ) {
-        wp_safe_redirect( home_url( '/' ), 301 );
+    if (
+        '/top/' === untrailingslashit( $request_uri ) . '/' ||
+        '/en/top-2/' === untrailingslashit( $request_uri ) . '/' ||
+        '8' === $page_id
+    ) {
+        $redirect_to = '/en/top-2/' === untrailingslashit( $request_uri ) . '/' ? home_url( '/en/' ) : home_url( '/' );
+        wp_safe_redirect( $redirect_to, 301 );
         exit;
     }
 }
