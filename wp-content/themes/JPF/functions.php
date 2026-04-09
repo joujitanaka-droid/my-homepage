c6f4acc0389072fa7449d353b85f604c26081c59
田中丈治 <jpf-30568@leto.eonet.ne.jp>
2026-04-09 10:20:43 +0900

    Change Japanese top hero text to 業務内容

--- wp-content/themes/JPF/functions.php
+++ wp-content/themes/JPF/functions.php
@@ -17,6 +17,7 @@ add_filter( 'pre_get_document_title', 'jpf_english_slowth_document_title' );
 add_filter( 'redirect_canonical', 'jpf_disable_english_slowth_canonical_redirect', 10, 2 );
 add_filter( 'pll_check_canonical_url', 'jpf_disable_english_slowth_polylang_canonical' );
 add_filter( 'the_content', 'jpf_append_japanese_slowth_test_videos', 20 );
+add_filter( 'the_content', 'jpf_replace_japanese_top_hero_text', 25 );
 add_action( 'init', 'jpf_force_redirect_english_top2', 1 );
 add_action( 'template_redirect', 'jpf_force_render_english_home', 0 );
 
@@ -52,6 +53,19 @@ function jpf_is_english_home_request() {
     }
 
     return '/en/' === untrailingslashit( $path ) . '/';
+}
+
+function jpf_is_japanese_home_request() {
+    $request_uri = isset( $_SERVER['REQUEST_URI'] ) ? wp_unslash( $_SERVER['REQUEST_URI'] ) : '';
+    $path        = wp_parse_url( $request_uri, PHP_URL_PATH );
+
+    if ( ! is_string( $path ) ) {
+        return false;
+    }
+
+    $normalized_path = untrailingslashit( $path ) . '/';
+
+    return '/' === $normalized_path || '/top/' === $normalized_path || '/tophurui/' === $normalized_path;
 }
 
 function jpf_get_normalized_path( $url ) {
@@ -427,6 +441,18 @@ function jpf_append_japanese_slowth_test_videos( $content ) {
 HTML;
 
     return $content . $video_section;
+}
+
+function jpf_replace_japanese_top_hero_text( $content ) {
+    if ( is_admin() || ! is_main_query() || ! in_the_loop() ) {
+        return $content;
+    }
+
+    if ( ! jpf_is_japanese_home_request() ) {
+        return $content;
+    }
+
+    return str_replace( '金属部品加工', '業務内容', $content );
 }
 
 add_action( 'template_redirect', 'jpf_redirect_legacy_top_page' );
